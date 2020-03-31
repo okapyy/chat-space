f80913b577a5e4a5f5ae60f884d777f569258804
okapyy <m9_17g_suki@yahoo.co.jp>
2020-03-31 14:13:37 +0900

    メッセージ送信機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :group_users
   has_many :groups, through: :group_users
+  has_many :messages
+
   validates :nickname, presence: true, uniqueness: true
 end
